5ca9c30babf377ead209ee8d29fc4916e1a2f91f
gorkhali <gorkhali9999@gmail.com>
2020-09-05 12:36:32 +0545

    Updates

---
+++ precache-manifest.54a9a2f8873d4f8d6fbd6f12b0d1f148.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac625898d43af05f99aa4039d2baa944",
+    "url": "/github.io/React-Note-App/index.html"
+  },
+  {
+    "revision": "82613e26273d7c3c47bb",
+    "url": "/github.io/React-Note-App/static/css/main.27ada87d.chunk.css"
+  },
+  {
+    "revision": "5a8b65670520515c7533",
+    "url": "/github.io/React-Note-App/static/js/2.79510676.chunk.js"
+  },
+  {
+    "revision": "c3a1273baccf65a7783f8d3bab38b7e3",
+    "url": "/github.io/React-Note-App/static/js/2.79510676.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82613e26273d7c3c47bb",
+    "url": "/github.io/React-Note-App/static/js/main.49b1a4c0.chunk.js"
+  },
+  {
+    "revision": "6f1f254a08c0cb7732b2",
+    "url": "/github.io/React-Note-App/static/js/runtime-main.f2b05a8c.js"
+  }
+]);
